e7640e7ab467fc1a5c7656a12d06f0fe162628d7
hovokoch <hovoqoch@gmail.com>
2021-12-13 17:45:42 +0400

    Create games and uploads folder if does not exists

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,8 +1,19 @@
 const path = require('path')
 const multer = require('multer')
+const fs = require('fs')
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        let fs = require('fs'),
+            uploads_folder = './uploads',
+            games_folder = './games'
+
+        if (!fs.existsSync(uploads_folder)){
+            fs.mkdirSync(uploads_folder);
+        }
+        if (!fs.existsSync(games_folder)){
+            fs.mkdirSync(games_folder);
+        }
         cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
