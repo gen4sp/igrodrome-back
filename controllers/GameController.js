a32431de684a48623bcb3b13f4598dce6ec07fc0
Oleg Mikhailov <s2ferr@gmail.com>
2021-12-20 19:38:40 +0300

    firebase cloud upload

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -5,214 +5,216 @@ const unzipper = require('unzipper')
 
 // Confirmed games
 const confirmed = (req, res, next) => {
-    Game.find({status: 1}, function (err, games) {
-        if (err) {
-            res.json({
-                status: 'error',
-                message: 'Что-то пошло не так!'
-            })
-        } else {
-            res.json({
-                games
-            })
-        }
-    })
+  Game.find({ status: 1 }, function(err, games) {
+    if (err) {
+      res.json({
+        status: 'error',
+        message: 'Что-то пошло не так!'
+      })
+    } else {
+      res.json({
+        games
+      })
+    }
+  })
 }
 
 // Creator games
 const myGames = (req, res) => {
-    Game.count({creator_id: req.user.id}, function (err, total) {
+  Game.count({ creator_id: req.user.id }, function(err, total) {
+    if (err) {
+      res.json({
+        status: 'error',
+        message: 'Что-то пошло не так!'
+      })
+    } else {
+      Game.find({ creator_id: req.user.id }, function(err, games) {
         if (err) {
-            res.json({
-                status: 'error',
-                message: 'Что-то пошло не так!'
-            })
+          res.json({
+            status: 'error',
+            message: 'Что-то пошло не так!'
+          })
         } else {
-            Game.find({creator_id: req.user.id}, function (err, games) {
-                if (err) {
-                    res.json({
-                        status: 'error',
-                        message: 'Что-то пошло не так!'
-                    })
-                } else {
-                    res.json({
-                        games,
-                        total
-                    })
-                }
-            })
+          res.json({
+            games,
+            total
+          })
         }
-    })
+      })
+    }
+  })
 }
 
 // All games
 const index = (req, res, next) => {
-    Game.count()
-        .then(total => {
-            Game.find()
-                .then(response => {
-                    res.json({
-                        response,
-                        total
-                    })
-                })
-                .catch(error => {
-                    res.json({
-                        status: 'error',
-                        message: 'Что-то пошло не так!'
-                    })
-                })
+  Game.count().then(total => {
+    Game.find()
+      .then(response => {
+        res.json({
+          response,
+          total
         })
+      })
+      .catch(error => {
+        res.json({
+          status: 'error',
+          message: 'Что-то пошло не так!'
+        })
+      })
+  })
 }
 
 // Single
 const show = (req, res, next) => {
-    let gameID = req.body.id
+  let gameID = req.body.id
 
-    Game.findById(gameID)
-        .then(response => {
-            res.json({
-                response
-            })
-        })
-        .catch(error => {
-            res.json({
-                status: 'error',
-                message: 'Что-то пошло не так!'
-            })
-        })
+  Game.findById(gameID)
+    .then(response => {
+      res.json({
+        response
+      })
+    })
+    .catch(error => {
+      res.json({
+        status: 'error',
+        message: 'Что-то пошло не так!'
+      })
+    })
 }
 
 // Create
 const store = (req, res, next) => {
+  let game = new Game({
+    title: req.body.title,
+    status: req.body.status === 'true',
+    creator_id: req.body.creator_id,
+    owner_id: req.body.owner_id
+  })
 
-    let game = new Game({
-        title: req.body.title,
-        status: req.body.status === 'true',
-        creator_id: req.body.creator_id,
-        owner_id: req.body.owner_id,
+  if (req.file) {
+    game.file = req.file.path
+  } else {
+    res.json({
+      status: 'error',
+      message: 'Пожалуйста загрузите файл!'
     })
+    return
+  }
 
-    if (req.file) {
-        game.file = req.file.path
-    } else {
-        res.json({
-            status: 'error',
-            message: 'Пожалуйста загрузите файл!'
-        })
-        return
-    }
-
-    game.save()
-        .then(game => {
-            if (game.file) {
-                fs.createReadStream(req.file.path).pipe(unzipper.Extract({path: 'games/' + game.slug}))
-
-                // fs.readFile('games/'+game.slug+'/index.html', 'utf8', function (err,data) {
-                //     if (err) {
-                //         return console.log(111, err);
-                //     }
-                //     let result = data.replace(/src="js/g, 'src="http://localhost:4000/games/'+game.slug+'/js');
-                //
-                //     fs.writeFile('games/'+game.slug+'/index.html', result, 'utf8', function (err) {
-                //         if (err) return console.log(222 ,err);
-                //     });
-                //     console.log(9999)
-                // });
-            }
-            res.json({
-                status: 'success',
-                message: 'Игра успешно добавлена'
-            })
-        })
-        .catch(error => {
-            console.log(error)
-            res.json({
-                status: 'error',
-                message: 'Произошла ошибка!'
-            })
-        })
+  game
+    .save()
+    .then(game => {
+      //       if (game.file) {
+      //         fs.createReadStream(req.file.path).pipe(unzipper.Extract({ path: 'games/' + game.slug }))
+      //
+      //         // fs.readFile('games/'+game.slug+'/index.html', 'utf8', function (err,data) {
+      //         //     if (err) {
+      //         //         return console.log(111, err);
+      //         //     }
+      //         //     let result = data.replace(/src="js/g, 'src="http://localhost:4000/games/'+game.slug+'/js');
+      //         //
+      //         //     fs.writeFile('games/'+game.slug+'/index.html', result, 'utf8', function (err) {
+      //         //         if (err) return console.log(222 ,err);
+      //         //     });
+      //         //     console.log(9999)
+      //         // });
+      //       }
+      res.json({
+        status: 'success',
+        message: 'Игра успешно добавлена'
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.json({
+        status: 'error',
+        message: 'Произошла ошибка!'
+      })
+    })
 }
 
 // Update
 const update = (req, res, next) => {
-    let gameID = req.body.id,
-        updateData = {
-            title: req.body.title,
-            status: req.body.status === 'true',
-            creator_id: req.body.creator_id,
-            owner_id: req.body.owner_id,
-        }
-
-    if (req.file) {
-        updateData.file = req.file.path
+  let gameID = req.body.id,
+    updateData = {
+      title: req.body.title,
+      status: req.body.status === 'true',
+      creator_id: req.body.creator_id,
+      owner_id: req.body.owner_id
     }
 
-    Game.findByIdAndUpdate(gameID, {$set: updateData},{upsert: true, new: true}, function (err, game) {
-        if (err) {
-            res.json({
-                status: 'error',
-                message: 'Произошла ошибка!'
-            })
-        } else {
-            if (req.file) {
-                fs.createReadStream(req.file.path).pipe(unzipper.Extract({path: 'games/' + game.slug}))
-            }
-            res.json({
-                status: 'success',
-                message: 'Игра успешно обновлена'
-            })
-        }
-    })
+  if (req.file) {
+    updateData.file = req.file.path
+  }
+
+  Game.findByIdAndUpdate(gameID, { $set: updateData }, { upsert: true, new: true }, function(
+    err,
+    game
+  ) {
+    if (err) {
+      res.json({
+        status: 'error',
+        message: 'Произошла ошибка!'
+      })
+    } else {
+      if (req.file) {
+        fs.createReadStream(req.file.path).pipe(unzipper.Extract({ path: 'games/' + game.slug }))
+      }
+      res.json({
+        status: 'success',
+        message: 'Игра успешно обновлена'
+      })
+    }
+  })
 }
 
 // Delete
 const destroy = (req, res, next) => {
-    let gameID = req.body.id
+  let gameID = req.body.id
 
-    Game.findByIdAndRemove(gameID)
-        .then(response => {
-            res.json({
-                message: 'Игра успешно удалена'
-            })
-        })
-        .catch(error => {
-            res.json({
-                message: 'Что-то пошло не так!'
-            })
-        })
+  Game.findByIdAndRemove(gameID)
+    .then(response => {
+      res.json({
+        message: 'Игра успешно удалена'
+      })
+    })
+    .catch(error => {
+      res.json({
+        message: 'Что-то пошло не так!'
+      })
+    })
 }
 
 // Get game html
 const getGame = (req, res) => {
-    const filePath = path.join(__dirname + './../games/' + req.query.slug + '/index.html');
-    res.sendFile(filePath);
+  const filePath = path.join(__dirname + './../games/' + req.query.slug + '/index.html')
+  res.sendFile(filePath)
 }
 
 // Check status
 const checkStatus = (req, res) => {
-    Game.find({slug: req.query.slug}, function (err, game) {
-        if (err) {
-            res.json({
-                status: 'error',
-                message: 'Что-то пошло не так!'
-            })
-        } else {
-            res.json({
-                game
-            })
-        }
-    })
+  Game.find({ slug: req.query.slug }, function(err, game) {
+    if (err) {
+      res.json({
+        status: 'error',
+        message: 'Что-то пошло не так!'
+      })
+    } else {
+      res.json({
+        game
+      })
+    }
+  })
 }
 
 module.exports = {
-    confirmed,
-    myGames,
-    index,
-    show,
-    store,
-    update,
-    destroy,
-    getGame,
-    checkStatus
+  confirmed,
+  myGames,
+  index,
+  show,
+  store,
+  update,
+  destroy,
+  getGame,
+  checkStatus
 }
--- middleware/upload.js
+++ middleware/upload.js
@@ -1,39 +1,45 @@
 const path = require('path')
 const multer = require('multer')
 const fs = require('fs')
+const FirebaseStorage = require('multer-firebase-storage')
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        let fs = require('fs'),
-            uploads_folder = './uploads',
-            games_folder = './games'
-
-        if (!fs.existsSync(uploads_folder)){
-            fs.mkdirSync(uploads_folder);
-        }
-        if (!fs.existsSync(games_folder)){
-            fs.mkdirSync(games_folder);
-        }
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        let ext = path.extname(file.originalname)
-        cb(null, Date.now() + ext)
-    }
-})
+// let storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         let fs = require('fs'),
+//             uploads_folder = './uploads',
+//             games_folder = './games'
+//
+//         if (!fs.existsSync(uploads_folder)){
+//             fs.mkdirSync(uploads_folder);
+//         }
+//         if (!fs.existsSync(games_folder)){
+//             fs.mkdirSync(games_folder);
+//         }
+//         cb(null, 'uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//         let ext = path.extname(file.originalname)
+//         cb(null, Date.now() + ext)
+//     }
+// })
 
 let upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, callback) {
-        if(file.mimetype === 'application/x-zip-compressed' || file.mimetype === 'application/zip'){
-            callback(null, true)
-        }else{
-            callback(null, false)
-        }
-    },
-    limits: {
-        fileSize: 1024 * 1024 * 25
+  storage: FirebaseStorage({
+    bucketName: 'gs://manitou-alfa-v1.appspot.com',
+    credentials: require('../firebase-service-key.json'),
+    // directoryPath: 'games',
+    unique: true
+  }),
+  fileFilter: function(req, file, callback) {
+    if (file.mimetype === 'application/x-zip-compressed' || file.mimetype === 'application/zip') {
+      callback(null, true)
+    } else {
+      callback(null, false)
     }
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 25
+  }
 })
 
 module.exports = upload
