3d3a392b9c8990d953d7d08c4c2c4288e535e1ce
Oleg Mikhailov <s2ferr@gmail.com>
2021-12-24 21:01:51 +0300

    state statuses; state in game;

--- src/models/Game.js
+++ src/models/Game.js
@@ -3,31 +3,19 @@ const slug = require('mongoose-slug-generator')
 const Schema = mongoose.Schema
 mongoose.plugin(slug)
 
-const gameSchema = new Schema({
-    title: {
-        type: String
-    },
-    slug: {
-        type: String,
-        slug: "title",
-        unique: true
-    },
-    status: {
-        type: Boolean
-    },
-    owner_id: {
-        type: String
-    },
-    creator_id: {
-        type: String
-    },
-    data: {
-        type: String
-    },
-    file: {
-        type: String
-    },
-}, {timestamps: true})
+const gameSchema = new Schema(
+  {
+    title: { type: String },
+    slug: { type: String, slug: 'title', unique: true },
+    status: { type: Boolean },
+    state: { type: Schema.Types.ObjectId, ref: 'GameState' },
+    owner_id: { type: String },
+    creator_id: { type: String },
+    data: { type: String },
+    file: { type: String }
+  },
+  { timestamps: true }
+)
 
 const Game = mongoose.model('Game', gameSchema)
 
--- src/models/GameState.js
+++ src/models/GameState.js
@@ -3,17 +3,47 @@ import mongoose from 'mongoose'
 export const CREATED = 'CREATED'
 export const FETCHING = 'FETCHING'
 export const FETCHED = 'FETCHED'
+export const PREPARING = 'PREPARING'
+export const PREPARED = 'PREPARED'
+export const BUILDING = 'BUILDING'
+export const BUILDED = 'BUILDED'
+export const DEPLOYING = 'DEPLOYING'
+export const DEPLOYED = 'DEPLOYED'
+export const INITING = 'INITING'
+export const INITED = 'INITED'
 export const INTERRUPTED = 'INTERRUPTED'
+export const FAILED = 'FAILED'
+export const DONE = 'DONE'
 
 const Schema = mongoose.Schema
 
 const gameStateSchema = new Schema(
   {
-    game_id: { type: Schema.Types.ObjectId, ref: 'Game' },
-    state: { type: String, enum: [CREATED, FETCHING, FETCHED, INTERRUPTED], default: CREATED },
-    state_history: [{ type: String }],
-    log: [{ type: String }],
-    error: { type: String, default: null }
+    game: { type: Schema.Types.ObjectId, ref: 'Game' },
+    status: {
+      type: String,
+      enum: [
+        CREATED,
+        FETCHING,
+        FETCHED,
+        PREPARING,
+        PREPARED,
+        BUILDED,
+        BUILDING,
+        DEPLOYING,
+        DEPLOYED,
+        INITING,
+        INITED,
+        INTERRUPTED,
+        FAILED,
+        DONE
+      ],
+      default: CREATED
+    },
+    status_history: { type: Array, default: [], select: false },
+    context: { type: Object, default: {}, select: false },
+    log: { type: Array, default: [], select: false },
+    error: { type: Schema.Types.Mixed, default: null, select: false }
   },
   { timestamps: true }
 )
